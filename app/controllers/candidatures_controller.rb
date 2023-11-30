9ede60c8398320ffe7838297ae802941f1d0f07d
Daniel Neves de Oliveira <daniel.oliveira.au@hotmail.com>
2023-11-30 19:53:42 -0300

    feat: i18n translation #12

--- app/controllers/candidatures_controller.rb
+++ app/controllers/candidatures_controller.rb
@@ -1,5 +1,22 @@
 class CandidaturesController < InheritedResources::Base
 
+  def create
+    create!(notice: t('controllers.candidatures.create'))
+  end
+
+  def update
+    update!(notice: t('controllers.candidatures.update'))
+  end
+
+  def destroy
+    destroy! do |format|
+      format.html { 
+        redirect_to candidatures_path(status: :see_other),
+                    notice: "#{resource.company_name} #{t('controllers.candidatures.destroy')}" }
+    end
+  end
+
+
   private
 
   def candidature_params
