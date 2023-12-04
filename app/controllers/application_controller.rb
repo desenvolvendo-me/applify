c58aa56bec3ea4a382ac4086d8fb410f2e73d3d6
Cassio Santana <cassioo.roger@gmail.com>
2023-12-04 09:50:33 -0300

    feat(manager_profile): redirects user until profile is created
    ref #31

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :check_profile_completion, if: :user_signed_in?
+  before_action :check_profile_completion, if: lambda {
+    user_signed_in? && !creating_profile?
+  }
 
   private
 
   def check_profile_completion
-    redirect_to new_manager_profile_path unless current_user.profile.present?
+    redirect_to new_manager_profile_path if current_user.profile.blank?
+  end
+
+  def creating_profile?
+    controller_name == 'profiles' &&
+      (action_name == 'new' || action_name == 'create')
   end
 end
