490eb11acd883e599e81e54f06b0fba4e2c3fffa
Daniel Neves de Oliveira <daniel.oliveira.au@hotmail.com>
2023-12-05 17:51:35 -0300

    refactor: candidature

--- app/models/candidature.rb
+++ app/models/candidature.rb
@@ -9,10 +9,10 @@
 #  updated_at   :datetime         not null
 #
 class Candidature < ApplicationRecord
-  enum situation: { send: 0, sending: 1, answered: 2 }
+  enum :situation,  send: 0, sending: 1, answered: 2
 
-  def humanized_situation
-    I18n.t("simple_form.options.candidature.situation_#{situation}",
-           default: situation.to_s)
-  end
+  # def self.humanized_situation(situation)
+  #   I18n.t("simple_form.options.candidature.situation_#{situation}",
+  #          default: situation.to_s)
+  # end
 end
