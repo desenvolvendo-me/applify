f9d2e17118939eb9278886933264e64602af1941
Philipe Rodrigues <dev.philipe@outlook.com>
2023-12-06 14:11:23 -0300

    fix(job_simulation): changes interview_test model to job_simulation

---
+++ app/controllers/manager/job_simulations_controller.rb
@@ -0,0 +1,21 @@
+module Manager
+  class JobSimulationsController < InternalController
+    before_action :set_job_simulation, only: :show
+
+    def index
+      @q = JobSimulation.ransack(params[:q])
+      @job_simulations = @q.result(distinct: true)
+      @job_simulations = @job_simulations.order('created_at')
+                                         .page(params[:page])
+                                         .per(4)
+    end
+
+    def show; end
+
+    private
+
+    def set_job_simulation
+      @job_simulations = JobSimulation.find(params[:id])
+    end
+  end
+end
