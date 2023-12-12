1cda00147c8602ae01bd29210245ff750e50a3b5
Cassio Santana <cassioo.roger@gmail.com>
2023-12-12 15:25:44 -0300

    style(schema): removes blank line, corrects block closure and friction array
    Ref #31

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_12_06_192919) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,9 +23,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_06_192919) do
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
+    t.index %w[author_type author_id], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
+    t.index %w[resource_type resource_id], name: "index_active_admin_comments_on_resource"
   end
 
   create_table "admin_users", force: :cascade do |t|
@@ -85,6 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_06_192919) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["job_simulation_id"], name: "index_simulation_questions_on_job_simulation_id"
+  end
 
   create_table "stacks", force: :cascade do |t|
     t.string "name"
