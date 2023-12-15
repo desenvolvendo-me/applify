27a8ce2d521dc5e4127df2bafe990d3bcc27f544
Philipe Rodrigues <dev.philipe@outlook.com>
2023-12-15 11:16:21 -0300

    test(models): create and fix tests for job_simulation and simulation_question

--- spec/models/job_simulation_spec.rb
+++ spec/models/job_simulation_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe JobSimulation, type: :model do
     }
     it {
       should accept_nested_attributes_for(:simulation_questions)
-        .allow_destroy(true)
     }
   end
 end
--- spec/models/simulation_question_spec.rb
+++ spec/models/simulation_question_spec.rb
@@ -26,4 +26,22 @@ RSpec.describe SimulationQuestion, type: :model do
   describe 'associations' do
     it { should belong_to(:job_simulation) }
   end
+
+  describe 'validations' do
+    it {
+ should define_enum_for(:answer_type).with_values(answer_check: 0, 
+                                                  answer_text: 1, answer_link: 2, answer_file: 3) }
+  end
+
+  describe 'file attachment' do
+    it 'attaches a file' do
+      question = create(:simulation_question)
+      file_path = Rails.root.join('spec/fixtures/files/arquivo_exemplo.txt')
+
+      question.answer_file.attach(io: File.open(file_path),
+                                  filename: 'arquivo_exemplo.txt', content_type: 'text/plain')
+
+      expect(question.answer_file).to be_attached
+    end
+  end
 end
