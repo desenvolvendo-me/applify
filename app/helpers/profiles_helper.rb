d97d4776117ac4115a0e76ab0d3f7b56c1f6e7aa
Cassio Santana <cassioo.roger@gmail.com>
2023-12-27 19:12:00 -0300

    feat(profile): add the image to the profile menu
    Ref #47

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -7,4 +7,15 @@ module ProfilesHelper
       t('views.manager.profile.complete_registration_view.title')
     end
   end
+
+  def user_profile_image(current_user)
+    if current_user.profile.profile_picture.attached?
+      image_tag current_user.profile.profile_picture,
+                class: 'w-8 h-8 rounded-full'
+    else
+      content_tag(:svg, nil, class: 'w-8 h-8 rounded-full', fill: '#A0AEC0') do
+        content_tag(:rect, nil, width: '100%', height: '100%')
+      end
+    end
+  end
 end
