ca222158a133417fb713d6886d566240b2044fc8
ricardo-benicio <rikrdoofelipe@outlook.com>
2024-01-03 11:51:39 -0300

    refactor(I18n): change and implement i18n "Lazy Lookup".

--- config/initializers/simple_form_tailwind.rb
+++ config/initializers/simple_form_tailwind.rb
@@ -38,8 +38,23 @@ SimpleForm.setup do |config|
   # vertical forms
   #
   # vertical default_wrapper
-  config.wrappers :vertical_form, tag: 'div', class: 'relative z-0 w-full mr-2 mb-6 group mt-4' do |b|
+  config.wrappers :vertical_form, tag: 'div', class: 'relative z-0 w-1/2 mr-2 mb-6 group mt-4' do |b|
     # b.use :html5 #NOTE: Removido por atrapalhar a validação
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :minlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :input, class: 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer', valid_class: 'border-green-400'
+    b.use :label, class: 'peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6', error_class: 'text-red-500'
+    b.use :full_error, wrap_with: { tag: 'p', class: 'mt-2 text-red-500 text-xs italic' }
+    b.use :hint, wrap_with: { tag: 'p', class: 'mt-2 text-grey-700 text-xs italic' }
+  end
+
+  # vertical full_width_wrapper
+  config.wrappers :vertical_form_full_width, tag: 'div', class: 'relative z-0 w-full mr-2 mb-6 group mt-4' do |b|
+    b.use :html5
     b.use :placeholder
     b.optional :maxlength
     b.optional :minlength
