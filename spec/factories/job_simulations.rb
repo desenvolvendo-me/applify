c39800abce10258040e701f9ccbca92d3daac089
Philipe Rodrigues <dev.philipe@outlook.com>
2024-01-04 09:15:00 -0300

    test(job_simulation): add status to factorybot

--- spec/factories/job_simulations.rb
+++ spec/factories/job_simulations.rb
@@ -4,12 +4,14 @@
 #
 #  id         :bigint           not null, primary key
 #  name       :string
+#  status     :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 FactoryBot.define do
   factory :job_simulation do
     name { %w['Perguntas Versão 1' 'Perguntas Jr 1' 'Perguntas Jr 0' 'Perguntas Padrão'].sample }
+    status { 0 }
 
     trait :with_simulation_questions do
       after(:create) do |job_simulation, _evaluator|
