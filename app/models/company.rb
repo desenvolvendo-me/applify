b39e6cebc08d9865cb80e3186957b8d420f0f47f
ricardo-benicio <rikrdoofelipe@outlook.com>
2024-01-05 11:22:13 -0300

    feat: created company exclusion rule.

--- app/models/company.rb
+++ app/models/company.rb
@@ -12,4 +12,16 @@
 #
 class Company < ApplicationRecord
   has_and_belongs_to_many :stacks
+  has_many :candidatures, dependent: :restrict_with_exception
+
+  validate :not_associate_to_candidature, on: :destroy
+
+  private
+
+  def not_associate_to_candidature
+    return unless candidatures.exists?
+
+    errors.add(:base, 'Não possível excluir uma empresa associada a uma candidatura.')
+    throw :abort
+  end
 end
