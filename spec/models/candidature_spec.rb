bc92a81a07df6c754f65e92afb382c0e497e5981
ricardo-benicio <rikrdoofelipe@outlook.com>
2024-01-10 08:33:09 -0300

    test: refactored rspec Candidature and Company.

--- spec/models/candidature_spec.rb
+++ spec/models/candidature_spec.rb
@@ -15,6 +15,15 @@
 #  situation               :string
 #  created_at              :datetime         not null
 #  updated_at              :datetime         not null
+#  company_id              :bigint           not null
+#
+# Indexes
+#
+#  index_candidatures_on_company_id  (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
 #
 require 'rails_helper'
 
