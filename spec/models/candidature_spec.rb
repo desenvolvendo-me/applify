dbd66ce5bf28c61d9d600cef997f54c2af250e0a
Philipe Rodrigues <dev.philipe@outlook.com>
2024-01-11 10:16:38 -0300

    test(candidatures): model test belong to profile

--- spec/models/candidature_spec.rb
+++ spec/models/candidature_spec.rb
@@ -28,6 +28,8 @@
 require 'rails_helper'
 
 RSpec.describe Candidature, type: :model do
+  it { should belong_to(:profile) }
+
   describe 'validations' do
     it { should validate_presence_of(:company_name) }
   end
